5b1cf887e0014147bf78ba380f717580c80d0944
tflinch <tflinch16@gmail.com>
2024-08-28 09:47:00 -0400

    feat: add /auth/signin, verifies login and sends token

--- app.py
+++ app.py
@@ -47,6 +47,27 @@ def sign_token():
 
     return jsonify({"token": token_str})
 
+@app.route('/auth/signin', methods=["POST"])
+def signin():
+    try:
+        sign_in_form_data = request.get_json()
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM users WHERE username = %s;", (sign_in_form_data["username"],))
+        existing_user = cursor.fetchone()
+        if existing_user is None:
+            return jsonify({"error": "Invalid credentials."}), 401
+        password_is_valid = bcrypt.checkpw(bytes(sign_in_form_data["password"], 'utf-8'), bytes(existing_user["password"], 'utf-8'))
+        if not password_is_valid:
+            return jsonify({"error": "Invalid credentials."}), 401
+        
+        token = jwt.encode({"username": existing_user["username"], "id": existing_user["id"]}, os.getenv("JWT_SECRET"))
+        return jsonify({"message": "Successful credetnials.", "token": token}), 201
+    except Exception as error:
+        return jsonify({"error": "Invalid credentials."}), 401
+    finally:
+        connection.close()
+
 @app.route('/auth/signup', methods=['POST'])
 def signup():
     try:
@@ -63,7 +84,8 @@ def signup():
         created_user = cursor.fetchone()
         connection.commit()
         connection.close()
-        return jsonify(created_user), 201
+        token = jwt.encode(created_user, os.getenv('JWT_SECRET'))
+        return jsonify({"token": token, "user":created_user}), 201
     except Exception as error:
         return jsonify({"error": str(error)}), 401
 
